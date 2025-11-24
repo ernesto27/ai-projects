4d9f1a28c0d4843509df98decdeef2b3be4daf0a
ernesto <ernestoponce27@gmail.com>
2025-11-24 18:25:48 -0300

    update

--- build-llm/hello.py
+++ build-llm/hello.py
@@ -3,9 +3,66 @@ import re
 with open("The_Verdict.txt", "r", encoding="utf-8") as file:
     content = file.read()
 
-print("Total number of characters ", len(content))
-# print(content[:99])
 
-preprocessed = re.split(r'([,.:;?_!"()\']|--|\\s)', content)
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', content)
 preprocessed = [item.strip() for item in preprocessed if item.strip()]
 print(len(preprocessed))
+
+
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+
+vocab = {token: integer for integer, token in enumerate(all_tokens)}
+
+# for i, item in enumerate(list(vocab.items())[-5:]):
+#     print(item)
+
+
+
+# for i, item in enumerate(vocab.items()):
+#     print(item)
+#     if i >= 50:
+#         break
+
+class SimpleTokenizerV1:
+    def __init__(self, vocab): 
+        self.str_to_int = vocab
+        self.int_to_str = {i: s for s, i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+
+        preprocessed = [item if item in self.str_to_int else "<|unk|>" for item in preprocessed]
+
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids 
+    
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r"\s+([,\.?!\"'\(\)])", r"\1", text)
+        return text
+
+
+tokenizer = SimpleTokenizerV1(vocab)
+# text = """"It's the last he painted, you know,"
+# Mrs. Gisburn said with pardonable pride."""
+# ids = tokenizer.encode(text)
+# print(ids)
+
+# print(tokenizer.decode(ids))
+
+# text = "Hello, do you like tea?"
+# print(tokenizer.encode(text))
+
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+text = " <|endoftext|> ".join((text1, text2))
+print(text)
+
+tokenizer = SimpleTokenizerV1(vocab)
+print(tokenizer.encode(text))
+
+print(tokenizer.decode(tokenizer.encode(text)))
