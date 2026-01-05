4a71a2a8b73633850bfe4663d1147f2174e41dd0
ernesto <ernestoponce27@gmail.com>
2026-01-04 23:00:45 -0300

    update

---
+++ build-llm/05training.py
@@ -0,0 +1,336 @@
+import torch
+import tiktoken
+from llm import DummyGPTModel, FeedForward,LayerNorm, ExampleDeepNeuralNetwork, TransformerBlock, GPTModel
+
+
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        
+        logits = logits[:, -1, :] 
+        probas = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=1)
+    return idx
+
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,
+    "context_length": 256,
+    "emb_dim": 768,
+    "n_heads": 12,
+    "n_layers": 12,
+    "drop_rate": 0.1,
+    "qkv_bias": False
+}
+
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+model.eval()
+
+def text_to_token_ids(text, tokenizer):
+    encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+    return encoded_tensor
+
+def token_ids_to_text(token_ids, tokenizer):
+    flat = token_ids.squeeze(0)
+    return tokenizer.decode(flat.tolist())
+
+start_context = "Every effort moves you"
+tokenizer = tiktoken.get_encoding("gpt2")
+
+token_ids = generate_text_simple(
+    model=model, 
+    idx=text_to_token_ids(start_context, tokenizer),
+    max_new_tokens=10,
+    context_size=GPT_CONFIG_124M["context_length"]
+)
+
+print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+
+inputs = torch.tensor([
+    [16833, 3626, 6100], # ["every effort moves",
+    [40,1107, 588] # "I really like"]
+])
+
+targets = torch.tensor([
+    [3626, 6100, 345 ], # [" effort moves you",
+    [1107, 588, 11311]]) # " really like chocolate"]
+
+
+with torch.no_grad():
+    logits = model(inputs)
+probas = torch.softmax(logits, dim=-1)
+print(probas.shape)
+
+# token_ids = torch.argmax(probas, dim=-1, keepdim=True)
+# print("Token IDs:\n", token_ids)
+
+# print(f"Target batch1: {token_ids_to_text(targets[0], tokenizer)}")
+# print(f"Output batch1: {token_ids_to_text(token_ids[0].flatten(), tokenizer)}")
+
+# text_idx = 0
+# target_probas_1 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+# print("Text 1:", target_probas_1)
+# text_idx = 1
+# target_probas_2 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+# print("Text 2:", target_probas_2)
+
+# log_probas = torch.log(torch.cat([target_probas_1, target_probas_2]))
+# print("Log probabilities:", log_probas)
+
+# avg_log_probas = torch.mean(log_probas)
+# print("Average log probability:", avg_log_probas.item())
+
+# neg_avg_log_probas = avg_log_probas * -1
+# print(neg_avg_log_probas)
+
+# logits_flat = logits.flatten(0, 1)
+# targets_flat = targets.flatten()
+# print("Flattened logits:", logits_flat.shape)
+# print("Flattened targets:", targets_flat.shape)
+
+# logs = torch.nn.functional.cross_entropy(logits_flat, targets_flat) 
+# print(logs)
+
+file_path = "The_Verdict.txt"
+with open(file_path, "r", encoding="utf-8") as f:
+    text_data = f.read()
+
+# total_characters = len(text_data)
+# total_tokens = len(tokenizer.encode(text_data))
+# print("Characters:", total_characters)
+# print("Tokens:", total_tokens)
+
+train_ratio = 0.90 
+split_idx = int(train_ratio * len(text_data)) 
+train_data = text_data[:split_idx]
+val_data = text_data[split_idx:]
+
+
+from bytepair import create_dataloader_v1 
+
+
+train_loader = create_dataloader_v1(
+    train_data, 
+    batch_size=2,
+    max_length=GPT_CONFIG_124M["context_length"], 
+    stride=GPT_CONFIG_124M["context_length"], 
+    drop_last=True, 
+    shuffle=True,
+    num_workers=0 
+)
+
+val_loader = create_dataloader_v1(
+    val_data,
+    batch_size=2,
+    max_length=GPT_CONFIG_124M["context_length"],
+    stride=GPT_CONFIG_124M["context_length"],
+    drop_last=False,
+    shuffle=False,
+    num_workers=0
+)
+
+
+# print("Train loader:")
+# for x, y in train_loader:
+#     print(x.shape, y.shape)
+
+# print("\nValidation loader:")
+# for x, y in val_loader:
+#     print(x.shape, y.shape)
+
+
+def calc_loss_batch(input_batch, traget_batch, model, device): 
+    input_batch = input_batch.to(device)
+    traget_batch = traget_batch.to(device)
+    logits = model(input_batch)
+    loss = torch.nn.functional.cross_entropy(
+        logits.flatten(0, 1), 
+        traget_batch.flatten()
+    )
+    return loss
+
+
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0.
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batches is None:
+        num_batches = len(data_loader)
+
+    else:
+        num_batches = min(num_batches, len(data_loader))
+    
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(
+                input_batch, target_batch, model, device
+            )
+            total_loss += loss.item()
+
+        else:
+            break
+    return total_loss / num_batches
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# model.to(device)
+# with torch.no_grad():
+#     train_loss = calc_loss_loader(train_loader, model, device)
+#     val_loss = calc_loss_loader(val_loader, model, device)
+# print("Train loss:", train_loss)
+# print("Validation loss:", val_loss)
+
+
+def train_model_simple(model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter, start_context, tokenizer):
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1
+
+    for epoch in range(num_epochs):
+        model.train() 
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(
+                input_batch, target_batch, model, device
+            )
+            loss.backward()
+            optimizer.step()
+            tokens_seen += input_batch.numel()
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter
+                )
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen) 
+                print(f"Ep {epoch+1} (Step {global_step:06d}): "
+                    f"Train loss {train_loss:.3f}, "
+                    f"Val loss {val_loss:.3f}"
+                )
+        generate_and_print_sample(
+            model, tokenizer, device, start_context
+        )
+    return train_losses, val_losses, track_tokens_seen
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+
+    with torch.no_grad():
+        train_loss = calc_loss_loader(
+            train_loader, model, device, eval_iter
+        )
+        val_loss = calc_loss_loader(
+            val_loader, model, device, eval_iter
+        )
+    model.train()
+    return train_loss, val_loss
+
+def generate_and_print_sample(model, tokenizer, device, start_context):
+    model.eval()
+    constext_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+    with torch.no_grad():
+        token_ids = generate_text_simple(
+            model=model, 
+            idx=encoded,
+            max_new_tokens=50,
+            context_size=constext_size
+        )
+    decoded_text = token_ids_to_text(token_ids, tokenizer)
+    print(decoded_text.replace("\n", " "))
+    model.train()
+
+
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+model.to(device)
+optimizer = torch.optim.AdamW(
+    model.parameters(), 
+    lr=0.0004, weight_decay=0.1
+)
+num_epochs= 10 
+
+# train_losses, val_losses, tokens_seen = train_model_simple(
+#     model, train_loader, val_loader, optimizer, device,
+#     num_epochs=num_epochs, eval_freq=5, eval_iter=5,
+#     start_context="Every effort moves you", tokenizer=tokenizer
+# )
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses): 
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+    ax1.plot(epochs_seen, train_losses, label="Train loss")
+    ax1.plot(epochs_seen, val_losses, linestyle="-.", label="Val loss")
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+    ax1.legend(loc="upper right")
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax2 = ax1.twiny()
+    ax2.plot(tokens_seen, train_losses, alpha=0)
+    ax2.set_xlabel("Tokens seen")
+    fig.tight_layout()
+    plt.show()
+
+# epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+# plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+
+
+model.to("cpu")
+model.eval()
+
+tokenizer = tiktoken.get_encoding("gpt2")
+token_ids = generate_text_simple(
+    model=model,
+    idx=text_to_token_ids("Every effort moves you", tokenizer),
+    max_new_tokens=25,
+    context_size=GPT_CONFIG_124M["context_length"]
+)
+
+# print("Final output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+
+vocab = {
+"closer": 0,
+"every": 1,
+"effort": 2,
+"forward": 3,
+"inches": 4,
+"moves": 5,
+"pizza": 6,
+"toward": 7,
+"you": 8,
+}
+inverse_vocab = {v: k for k, v in vocab.items()}
+
+next_token_logits = torch.tensor(
+[4.51, 0.89, -1.90, 6.75, 1.63, -1.62, -1.89, 6.28, 1.79]
+)
+
+probas = torch.softmax(next_token_logits, dim=0)
+# next_token_id = torch.argmax(probas).item()
+next_token_id = torch.multinomial(probas, num_samples=1).item()
+print(inverse_vocab[next_token_id])  # toward
+
+def print_sampled_tokens(probas):
+    torch.manual_seed(123)
+    smaple = [torch.multinomial(probas, num_samples=1).item() for i in range(1_000)]
+    sampled_ids = torch.bincount(torch.tensor(smaple))
+    for i, freq in enumerate(sampled_ids):
+        print(f"{freq} x {inverse_vocab[i]}")
+
+
+def softmax_with_temperature(logits, temperature):
+    scaled_logits = logits / temperature
+    return torch.softmax(scaled_logits, dim=0)
+
+print_sampled_tokens(probas)
